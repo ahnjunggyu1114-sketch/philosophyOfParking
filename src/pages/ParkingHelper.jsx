f7630fab0dc7ebcab36368b10892ad3d445485dd
안정규 <ahnjunggyu1114@gmail.com>
2026-05-18 14:58:36 +0900

    [FEAT] 백서버 주소 변경

--- src/pages/ParkingHelper.jsx
+++ src/pages/ParkingHelper.jsx
@@ -12,7 +12,7 @@ const ParkingHelper = () => {
     const [imagePreview, setImagePreview] = useState(null);
     const [parkingPoints, setParkingPoints] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
-    const API_URL = "https://isolated-plaza-broadcasting-jazz.trycloudflare.com/api/parking/analyze";
+    const API_URL = "https://api.mustashopping.shop/api/parking/analyze";
     const selectedVehicle = vehicleList[0];
 
     const handleImageUpload = (file) => {
--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://isolated-plaza-broadcasting-jazz.trycloudflare.com',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 })
